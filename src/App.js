1cba4ac79868a9cfda5eb5ef2c825d93412984ea
mutoromike <mikemutoro@gmail.com>
2020-07-01 17:50:44 +0300

    initiate develop branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Route, Switch, Router } from "react-router-dom";
 import "./App.css";
-import MyEvents from "./components/MyEvents/MyEvents";
-import Header from "./components/Header/Header";
-import Register from "./components/Register/Register";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import Profile from "./components/Profile/Profile";
-import PrivateRoute from "./components/Private/Private";
 import { createBrowserHistory } from "history";
 
 const history = createBrowserHistory();
@@ -16,12 +9,6 @@ const App = () => (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <PrivateRoute path="/myEvents" component={MyEvents} />
-        <Route path="/home" component={Home} />
-        <PrivateRoute path="/profile" component={Profile} />
       </Switch>
     </div>
   </Router>
